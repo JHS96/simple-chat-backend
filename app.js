99503a9dc219386ea65d5cba38cddf2cef6fabd8
Juan <jschaper@hotmail.co.za>
2020-11-27 15:34:19 +0200

    Security improvements with helmet and CORS setup.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const helmet = require('helmet');
+
 const authRoutes = require('./routes/auth');
 const contactRoutes = require('./routes/contact');
 const messageRoutes = require('./routes/messages');
@@ -12,7 +14,17 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(helmet());
+
 app.use(bodyParser.json());
+
+// CORS setup
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*'); // TODO change '*' to frontend's domain
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
+});
 
 app.use('/auth', authRoutes);
 app.use('/contact', contactRoutes);
